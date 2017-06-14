97beb3f9913395fee96defff1f1a77ef50d606ce
Michal K <mkrawetko@gmail.com>
2017-06-14 17:10:04 +0100

    Move mandatory configuration check to config.js. Clean some code

--- buildMonitor.js
+++ buildMonitor.js
@@ -1,18 +1,13 @@
-'use strict'
-var config = require('./config/config');
-var unirest = require('unirest');
-const http = require('http')  
+'use strict';
+let config = require('./config/config');
+let unirest = require('unirest');
+const http = require('http');
 
-console.log('config: ')
-console.log(config)
-console.log(config.teamcitybaseurl)
-
-
-const teamcity = config.teamcitybaseurl
-const port = 3000
+const teamcity = config.teamcitybaseurl;
+const port = 3000;
 
 
-var sys = require('util')
+var sys = require('util');
 var exec = require('child_process').exec;
 function logg(error, stdout, stderr) {
     console.log(stdout);
@@ -20,61 +15,59 @@ function logg(error, stdout, stderr) {
     console.log(error);
 }
 
-const requestHandler = (request, resp) => {  
-//  exec("ls -la",logg);
-    var buildStatus;
-    var storyNumber;
-    var whoToBlame;
-//  exec("say Great Success!",logg);
-    unirest.get(teamcity+"/app/rest/builds?locator=buildType:NetstreamTrunk_Faith_BuildMichalFaith,count:1").headers({'Accept': 'application/json'})
-.end(function (response) {
-  console.log(response.body);
-//        var jsonContent = JSON.parse(response.body);
-      console.log(response.body.build[0].href);
-        buildStatus=response.body.build[0].status
-         unirest.get(teamcity+response.body.build[0].href).headers({'Accept': 'application/json'})
-.end(function (response) {
-            console.log(response.body); 
-            console.log(response.body.changes.href);
-            unirest.get(teamcity+response.body.changes.href).headers({'Accept': 'application/json'})
-.end(function (response) {
-            console.log(response.body); 
-            console.log(response.body.change[0].href);
-             unirest.get(teamcity+response.body.change[0].href).headers({'Accept': 'application/json'})
-.end(function (response) {
-                 var comment = response.body.comment;
-            console.log(response.body); 
-            console.log(response.body.comment);
-            console.log(comment);
-                 var attr = comment.toString().split(" ");
-                 storyNumber = attr[0];
-                 whoToBlame = attr[1];
-                 console.log(storyNumber)
-                 console.log(whoToBlame)
-                 
-                 if(buildStatus==='FAILURE'){
-                      exec("say Build Failed! Shame on you "+whoToBlame,logg);  
+function checkStatus() {
+    let buildStatus;
+    let storyNumber;
+    let whoToBlame;
+    unirest.get(teamcity + "/app/rest/builds?locator=buildType:Provisioning_Tequila,count:1").headers({'Accept': 'application/json'})
+        .end(function (response) {
+            console.log(response.body);
+            console.log(response.body.build[0].href);
+            buildStatus = response.body.build[0].status;
+            unirest.get(teamcity + response.body.build[0].href).headers({'Accept': 'application/json'})
+                .end(function (response) {
+                    console.log(response.body);
+                    console.log(response.body.changes.href);
+                    unirest.get(teamcity + response.body.changes.href).headers({'Accept': 'application/json'})
+                        .end(function (response) {
+                            console.log(response.body);
+                            console.log(response.body.change[0].href);
+                            unirest.get(teamcity + response.body.change[0].href).headers({'Accept': 'application/json'})
+                                .end(function (response) {
+                                    let comment = response.body.comment;
+                                    console.log(response.body);
+                                    console.log(response.body.comment);
+                                    console.log(comment);
+                                    let attr = comment.toString().split(" ");
+                                    storyNumber = attr[0];
+                                    whoToBlame = attr[1];
+                                    console.log(storyNumber);
+                                    console.log(whoToBlame);
+
+                                    if (buildStatus === 'FAILURE') {
+                                        exec("say Build Failed! Shame on you " + whoToBlame, logg);
 //                       resp.end("Build Failed! shame on you "+whoToBlame)
-                 }else{
-                      exec("say Great Success! Good job "+ whoToBlame,logg);
+                                    } else {
+                                        exec("say Great Success! Good job " + whoToBlame, logg);
 //                       resp.end("Great Success! Good job "+ whoToBlame)
-                 }
-         }); 
-         }); 
-         });
-});
-  console.log(request.url);
-   resp.end("Hello world Node js! ")
-//                       resp.end("Great Success! Good job "+ whoToBlame)
-
+                                    }
+                                });
+                        });
+                });
+        });
 }
+checkStatus();
+const requestHandler = (request, resp) => {
+    checkStatus();
 
-const server = http.createServer(requestHandler)
+};
 
-server.listen(port, (err) => {  
-  if (err) {
-    return console.log('something bad happened', err)
-  }
+const server = http.createServer(requestHandler);
 
-  console.log(`server is listening on ${port}`)
-})
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
--- config/config.js
+++ config/config.js
@@ -1,3 +1,20 @@
-module.exports = {
-   teamcitybaseurl: process.env.teamcitybaseurl
-}
+'use strict';
+
+let config = function () {
+    let teamcitybaseurl = function () {
+        let teamcitybaseurl = process.env.teamcitybaseurl;
+
+        if (typeof teamcitybaseurl === 'undefined' || teamcitybaseurl === null) {
+            throw Error('IllegalArgumentException: teamcitybaseurl environment variable needs to be set, example:' +
+                ' http://testuser:testpassword@teamcity.jetbrains.com:8111')
+        }
+        return teamcitybaseurl;
+    }();
+
+    return {
+        teamcitybaseurl: teamcitybaseurl
+    }
+};
+
+
+module.exports = config();
