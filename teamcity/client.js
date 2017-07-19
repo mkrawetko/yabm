da67c859a7263239659257f7c37dd4bb02f914d2
Michal K <mkrawetko@gmail.com>
2017-07-19 18:16:02 +0100

    Start using nock for Rest tests instead of mocking unirest

--- teamcity/client.js
+++ teamcity/client.js
@@ -3,9 +3,13 @@ let unirest = require('unirest');
 
 let teamcityclient = function (baseUrl) {
 
+    const lasestBuildByTypeUrl = function (buildType) {
+        return baseUrl + `/app/rest/builds?locator=buildType:${buildType},count:1`;
+    };
+
     function getLatestBuildId(buildType) {
         return new Promise(function (resolve) {
-            unirest.get(baseUrl + `/app/rest/builds?locator=buildType:${buildType},count:1`)
+            unirest.get(lasestBuildByTypeUrl(buildType))
                 .headers({'Accept': 'application/json'})
                 .end(function (response) {
                     // console.log(response.body);
--- test/teamcity/client.spec.js
+++ test/teamcity/client.spec.js
@@ -1,51 +1,45 @@
 'use strict';
 const TeamCityClient = require('../../teamcity/client');
-const sinon = require('sinon');
+const nock = require('nock')
+;
 const chai = require('chai');
 chai.should();
-const unirest = require('unirest');
 
-const latestBuildResponse = {
-    "count": 1,
-    "href": "/app/rest/builds?locator=buildType:Provisioning_Tequila,count:1",
-    "nextHref": "/app/rest/builds?locator=buildType:Provisioning_Tequila,count:1,start:1",
-    "build": [{
-        "id": 10903080,
-        "buildTypeId": "Provisioning_Tequila",
-        "number": "521",
-        "status": "SUCCESS",
-        "state": "finished",
-        "href": "/app/rest/builds/id:10903080",
-        "webUrl": "http://teamcity.sns.sky.com:8111/viewLog.html?buildId=10903080&buildTypeId=Provisioning_Tequila"
-    }]
-};
 
 describe('teamcityClient', function () {
 
-    let teamcityClient = new TeamCityClient("http:///teamcityurl.com");
+    const TEAMCITY_BASE_URL = 'http://teamcityurl.com:8001';
+    const latestBuildResponse = {
+        "count": 1,
+        "href": "/app/rest/builds?locator=buildType:Provisioning_Tequila,count:1",
+        "nextHref": "/app/rest/builds?locator=buildType:Provisioning_Tequila,count:1,start:1",
+        "build": [{
+            "id": 10903080,
+            "buildTypeId": "Provisioning_Tequila",
+            "number": "521",
+            "status": "SUCCESS",
+            "state": "finished",
+            "href": "/app/rest/builds/id:10903080",
+            "webUrl": "http://teamcity.sns.sky.com:8111/viewLog.html?buildId=10903080&buildTypeId=Provisioning_Tequila"
+        }]
+    };
+
+    let teamcityClient = new TeamCityClient(TEAMCITY_BASE_URL);
 
 
     describe('get latest build by type: ', function () {
         beforeEach(function () {
-            this.get = sinon.stub(unirest, 'get');
-            console.log("beforeEach worked")
+            console.log("beforeEach worked");
+            nock.cleanAll();
         });
-        afterEach(function () {
-            this.get.restore();
-        });
+
         it('should return latest build id by build type ', function () {
-            let mockUnirest = {
-                headers: function () {
-                    return {
-                        end: function (cb) {
-                            cb({
-                                body: latestBuildResponse
-                            });
-                        }
-                    };
-                }
-            };
-            this.get.returns(mockUnirest);
+            nock(TEAMCITY_BASE_URL).log(console.log)
+                .matchHeader('accept', 'application/json')
+                .get('/app/rest/builds?locator=buildType:Provisioning_Tequila,count:1')
+                .reply(200, latestBuildResponse)
+            ;
+
             return teamcityClient.getLatestBuildId('Provisioning_Tequila')
                 .then(function (latestBuildLink) {
                         console.log(latestBuildLink);
