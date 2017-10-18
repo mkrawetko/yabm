ce2eb4204642801368574d0daf0c443e0b7f82ba
mkrawetko <mkrawetko@gmail.com>
2017-10-18 16:21:19 +0100

    Move BUILDS out of app.components.ts.  Remove gitignore and add package-lock.json
    remove gitignore since gitglobal is enough

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Build} from './build'
-
-const BUILDS: Build[] = [
-    {id: 'Tequila_Provisioning', name: 'Tequila Master Release', status: 'SUCCESS', path: "/Provisioning/Tequila"},
-    {id: 'Corona_Provisioning', name: 'Corona', status: 'SUCCESS', path: "/Provisioning/Corona"},
-    {id: 'HaloId', name: 'Halo', status: 'FAILED', path: "/Provisioning/Halo"},
-    {
-        id: 'SaltFaithId',
-        name: 'Run SALT Faith Tests',
-        status: 'SUCCESS',
-        path: "/Netstream/Netstream Trunk/Faith/Docker"
-    },
-    {id: 'IomFaithId', name: 'Run IOM Faith Tests', status: 'SUCCESS', path: "/Netstream/Netstream Trunk/Faith/Docker"},
-    {id: 'Halo_commonsId', name: 'Halo Commons', status: 'SUCCESS', path: "/Hiro Projects"}
-];
+import {BuildService} from "./build.service";
 
 const PROJECTS_GROUPS: string[] = [
     'Provisioning', 'Hiro Projects', 'Faith'
@@ -89,15 +76,26 @@ export class ProjectWithBuilds {
             line-height: 1em;
             position: relative;
         }
-    `]
+    `],
+    providers: [BuildService]
+
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    projectsWithBuilds: ProjectWithBuilds[];
+    failedBuilds: Build[];
 
-    projectsWithBuilds = PROJECTS_GROUPS.map(p => new ProjectWithBuilds(p, BUILDS
-            .filter(value => value.status === 'SUCCESS')
-            .filter(value => value.path.includes(`/${p}`))
-        )
-    );
+    ngOnInit(): void {
 
-    failedBuilds = BUILDS.filter(value => value.status === 'FAILED');
+        let allBuilds = this.buildsService.getBuilds();
+
+        this.projectsWithBuilds = PROJECTS_GROUPS.map(p => new ProjectWithBuilds(p, allBuilds
+                .filter(value => value.status === 'SUCCESS')
+                .filter(value => value.path.includes(`/${p}`))
+            )
+        );
+        this.failedBuilds = allBuilds.filter(value => value.status === 'FAILED');
+    }
+
+    constructor(private buildsService: BuildService) {
+    }
 }
--- angular/src/app/build.service.ts
+++ angular/src/app/build.service.ts
@@ -1,21 +1,9 @@
 import {Build} from "./build";
+import {BUILDS} from "./mock-builds";
 
-const BUILDS: Build[] = [
-    {id: 'Tequila_Provisioning', name: 'Tequila Master Release', status: 'SUCCESS', path: "/Provisioning/Tequila"},
-    {id: 'Corona_Provisioning', name: 'Corona', status: 'SUCCESS', path: "/Provisioning/Corona"},
-    {id: 'HaloId', name: 'Halo', status: 'FAILED', path: "/Provisioning/Halo"},
-    {
-        id: 'SaltFaithId',
-        name: 'Run SALT Faith Tests',
-        status: 'SUCCESS',
-        path: "/Netstream/Netstream Trunk/Faith/Docker"
-    },
-    {id: 'IomFaithId', name: 'Run IOM Faith Tests', status: 'SUCCESS', path: "/Netstream/Netstream Trunk/Faith/Docker"},
-    {id: 'Halo_commonsId', name: 'Halo Commons', status: 'SUCCESS', path: "/Hiro Projects"}
-];
 
 export class BuildService {
-    getBuilds(): Promise<Build[]> {
-        return Promise.resolve(BUILDS);
+    getBuilds(): Build[] {
+        return BUILDS;
     }
 }
